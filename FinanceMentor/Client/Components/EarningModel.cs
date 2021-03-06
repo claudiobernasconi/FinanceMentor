4fb6286df2d54a55803ff9f848c0614bc09da522
Claudio Bernasconi <email@claudio-bernasconi.net>
2021-03-06 23:03:21 +0100

    End of Episode 2 of the FREE Blazor Crash Course on YouTube

---
+++ FinanceMentor/Client/Components/EarningModel.cs
@@ -0,0 +1,22 @@
+﻿using FinanceMentor.Shared;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceMentor.Client.Components
+{
+    public class EarningModel
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Subject { get; set; }
+
+        [Required]
+        public EarningCategory Category { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
---
+++ FinanceMentor/Shared/EarningCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceMentor.Shared
+{
+    public enum EarningCategory
+    {
+        Salary,
+        CapitalGain,
+        Freelancing,
+        Coaching,
+        Flipping,
+        Gift,
+    }
+}
